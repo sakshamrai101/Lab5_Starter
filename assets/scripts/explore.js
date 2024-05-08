f408b3ad812cfc267382b6e7dc2745da69ebf10d
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-05-07 22:15:31 -0700

    testing branch: should be wrong

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,63 @@
-// explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const imgElement = document.querySelector('img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+  const synth = window.speechSynthesis;
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    // Clear existing options
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  talkButton.addEventListener('click', function () {
+    const textToSpeak = textArea.value;
+    if (synth.speaking) {
+      console.error('speechSynthesis.speaking');
+      return;
+    }
+
+    if (textToSpeak !== '') {
+      const utterance = new SpeechSynthesisUtterance(textToSpeak);
+      const selectedVoiceName = voiceSelect.selectedOptions[0].getAttribute('data-name');
+      utterance.voice = voices.find(voice => voice.name === selectedVoiceName);
+
+      utterance.onstart = function () {
+        imgElement.src = 'assets/images/smiling-open.png';  
+      };
+
+      utterance.onend = function () {
+        imgElement.src = 'assets/images/smiling.png';  
+      };
+
+      utterance.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+      };
+
+      synth.speak(utterance);
+    }
+  });
 }
